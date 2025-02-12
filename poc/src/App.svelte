77526bf73eb6464f61254e75e438cb1b5aff6b94
Eidamiino <adam.pala199@gmail.com>
2025-02-12 13:27:51 +0100

    header alignment improved

--- poc/src/App.svelte
+++ poc/src/App.svelte
@@ -126,8 +126,8 @@
 <div class="layout-wrapper">
   <div class="header">
     <div class="pure-g">
-      <div class="pure-u-1-2">
-        <div class="header-left">
+      <div class="pure-u-1-2 header-left">
+        <!-- <div class="header-left"> -->
           <button class="hamburger-btn" on:click={toggleSidebar}>
             <i class="fas fa-bars"></i>
           </button>
@@ -135,12 +135,12 @@
             <!-- <img class="pure-image" src="" alt="" /> -->
             <span>Dokumenty</span>
           </a>
-        </div>
+        <!-- </div> -->
       </div>
-      <div class="pure-u-1-2">
-        <div class="header-right">
+      <div class="pure-u-1-2 header-right">
+        <!-- <div class="header-right"> -->
           <a href="#" class="pure-menu-link">email@email.com</a>
-        </div>
+        <!-- </div> -->
       </div>
     </div>
   </div>
@@ -152,34 +152,33 @@
           <ul class="pure-menu-list">
             <li class="pure-menu-item first-item">
               <a href="#" class="pure-menu-link">
-                <i class="fas fa-dashboard"></i><span>Nástěnka</span>
+                <i class="fas fa-dashboard"></i>
+                <span>Nástěnka</span>
               </a>
             </li>
             <li class="pure-menu-item">
               <a href="#" class="pure-menu-link">
-                <i class="fas fa-file"></i><span>Dokumenty</span>
+                <i class="fas fa-file"></i>
+                <span>Dokumenty</span>
               </a>
             </li>
             <li class="pure-menu-item">
               <a href="#" class="pure-menu-link">
-                <i class="fas fa-layer-group"></i><span>Zařazení</span>
+                <i class="fas fa-layer-group"></i>
+                <span>Zařazení</span>
               </a>
             </li>
             <li class="pure-menu-item">
               <a href="#" class="pure-menu-link">
-                <i class="fas fa-list"></i><span>Umístění</span>
-              </a>
-            </li>
-
-            <li class="pure-menu-item">
-              <a href="#" class="pure-menu-link">
-                <i class="fas fa-users-viewfinder"></i><span>Objekty</span>
+                <i class="fas fa-list"></i>
+                <span>Umístění</span>
               </a>
             </li>
 
             <li class="pure-menu-item">
               <a href="#" class="pure-menu-link" on:click={toggleMenu}>
-                <i class="fas fa-cogs"></i><span>Administrace</span>
+                <i class="fas fa-cogs"></i>
+                <span>Administrace</span>
                 {#if !isSidebarHidden}
                   <i
                     class="fas fa-chevron-down arrow-icon {isSubmenuOpen
@@ -200,6 +199,13 @@
                 </li>
               </ul>
             {/if}
+
+            <li class="pure-menu-item">
+              <a href="#" class="pure-menu-link">
+                <i class="fas fa-users-viewfinder"></i>
+                <span>Objekty</span>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
